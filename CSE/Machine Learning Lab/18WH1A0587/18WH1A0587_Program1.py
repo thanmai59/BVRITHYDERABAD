531fbd54d74366d895c72f10763b8e25b9cd49c2
divyamalreddy <69200037+divyamalreddy@users.noreply.github.com>
2021-05-01 14:15:59 +0530

    Add files via upload

---
+++ CSE/Machine Learning Lab/18WH1A0587/18WH1A0587_Program1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+probAbsentFriday=0.03
+probFriday=0.2
+
+
+# In[3]:
+
+
+bayesResult = (probAbsentFriday/probFriday)
+print(bayesResult *100)
+
+
+# In[ ]:
+
+
+
+
