d8db12b3c94e9d82c602f4889ee59d9aa46404e0
saba185a4 <shaiksaba379@gmail.com>
2021-05-01 19:44:35 +0530

    Add files via upload

---
+++ CSE/Machine Learning Lab/18WH1A05A4/18wh1a05a4_Program1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+
+
+# In[9]:
+
+
+probStudAbsentAndFriday = 0.03
+probFriday = 0.2
+bayesResult = ( probStudAbsentAndFriday / probFriday )
+print(bayesResult * 100)
+
+
+# In[ ]:
+
+
+
+
