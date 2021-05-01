510a1561d30315fdd7e175e3323a65949b01773a
Mary Pranavi Allam <69451929+18WH1A0563@users.noreply.github.com>
2021-05-01 19:31:52 +0530

    Add files via upload

--- CSE/Machine Learning Lab/18WH1A0563/18WH1A0563_Program1.py
+++ CSE/Machine Learning Lab/18WH1A0563/18WH1A0563_Program1.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#program_1
+prbSAbsAndFri = 0.03
+probFri = 0.2
+bayesRes = (prbSAbsAndFri / probFri)
+print(bayesRes * 100)
+
+
+# In[ ]:
+
+
+
 
