702035ffd043e00d9f1e15d943b7a440738d6421
navyasree-583 <51987336+navyasree-583@users.noreply.github.com>
2021-05-01 13:30:58 +0530

    Add files via upload

--- CSE/Machine Learning Lab/18WH1A0583/18WH1A0583_Program1.py
+++ CSE/Machine Learning Lab/18WH1A0583/18WH1A0583_Program1.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#p(absent^friday)=0.03
+probAbsandFriday=0.03
+#p(friday)=0.2
+probFri=0.2
+#using Bayes Theorem
+# p(a/f) =p(a^f)/p(f)
+result=(probAbsandFriday/probFri)
+print(result * 100)
+
+
+# In[ ]:
+
+
+
 
