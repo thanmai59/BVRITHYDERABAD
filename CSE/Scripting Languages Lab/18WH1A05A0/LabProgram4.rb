8852a07078aaccb66ec0f68ce3be7d818b8c2c04
T-SravaniReddy <69198531+T-SravaniReddy@users.noreply.github.com>
2021-07-08 22:08:49 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A05A0/LabProgram4.rb
@@ -0,0 +1,19 @@
+#file = "/home/sravani/Desktop/SL/LabProgram3.rb"
+puts "Enter the file name"
+file = gets.chomp
+
+#file name
+fbname = File.basename file
+puts "File Name : "+fbname
+
+#base name
+bname = File.basename file,".rb"
+puts "Base Name : "+bname
+
+#file extension 
+fextn = File.extname file
+puts "File Extension : "+fextn
+
+#path name
+pathname = File.dirname file
+puts "Path Name : "+pathname
