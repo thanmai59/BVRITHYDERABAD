2172432679ba655b4ef71c6a6b42c657536cff66
shilparani900 <51986971+shilparani900@users.noreply.github.com>
2021-07-08 22:15:48 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A0577/program5.rb
@@ -0,0 +1,14 @@
+puts "Enter the 1st no: "
+x = gets.to_i
+puts "Enter the 2nd no: "
+y = gets.to_i
+puts "Enter the 3rd no: "
+z = gets.to_i
+
+if x >= y and x >= z
+   puts "x = #{x} is the greatest number."
+elsif y >= z and y >= x
+   puts "y = #{y} is the greatest number."
+else
+   puts "z = #{z} is the greatest number."
+end
