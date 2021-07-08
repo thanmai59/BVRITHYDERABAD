c495183ae8f4e210b1e7949d4900753e00793029
divyamalreddy <69200037+divyamalreddy@users.noreply.github.com>
2021-07-08 21:06:21 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A0587/program5.rb
@@ -0,0 +1,14 @@
+#write the ruby program to find greatest of 3 numbers
+
+puts "Enter the three numbers: "
+num1,num2,num3 = gets.split.map(&:to_i)
+#num2 = gets.chomp.to_i
+#num3 = gets.chomp.to_i
+
+if num1 > num2 and num1 > num3
+	puts "#{num1} is larger than #{num2} and #{num3}"
+elsif num2 > num3 and num2 > num1
+	puts "#{num2} is larger than #{num1} and #{num3}"
+else
+	puts "#{num3} is larger than #{num1} and #{num2}"
+end
