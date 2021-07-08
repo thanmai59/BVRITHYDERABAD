87d9c783787114d0a3d29b34b5234b258b08605f
TetaliVarshitha <69201555+TetaliVarshitha@users.noreply.github.com>
2021-07-08 20:28:27 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A0570/program3.rb
@@ -0,0 +1,9 @@
+# Write a Ruby script which accept the user's first and last name and print them in reverse order with a space between them
+
+puts "Input your first name:"
+fname = gets.chomp
+
+puts "Input your last name:"
+lname = gets.chomp
+
+puts "Hello #{lname} #{fname}"
