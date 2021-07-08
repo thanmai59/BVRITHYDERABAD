a804a708c914251502d55c06ee934c95d4b7e2a3
rathnachalla <68558385+rathna30@users.noreply.github.com>
2021-07-08 20:08:03 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A05A1/program2.rb
@@ -0,0 +1,10 @@
+#printing perimeter and area of a circle
+
+perimeter = 0.0
+area = 0.0
+print "Input the radius of the circle:"
+radius = gets.to_f
+perimeter = 2*3.147*radius
+area = 3.147*radius*radius
+print "The perimeter is : #{perimeter}.\n"
+print "The area is : #{area}."
