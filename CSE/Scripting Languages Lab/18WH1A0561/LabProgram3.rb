4b15390d27d0c24720320d34b79d6c1ec5a674e6
vyshalireddybanda <69138399+vyshalireddybanda@users.noreply.github.com>
2021-07-08 20:18:36 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A0561/LabProgram3.rb
@@ -0,0 +1,9 @@
+puts "Enter first name: "
+fname = gets.chomp
+puts "Enter last name: "
+lname = gets.chomp
+puts "Before reversing: "
+puts "#{fname} #{lname}"
+puts "After reversing: "
+puts "#{lname} #{fname}"
+
