8db9728b4e98e62b721ec5c70943459b2b1a2f40
AmulyaChandhanaVempati <51986500+AmulyaChandhanaVempati@users.noreply.github.com>
2021-07-08 21:25:53 +0530

    Delete CSE/Scripting Languages Lab/18WH1A05A3 directory

--- CSE/Scripting Languages Lab/18WH1A05A3/MultipleString.rb
+++
@@ -1,8 +0,0 @@
-def multiple_string(str, n)
-return str*n
-end
-print multiple_string('amulya ', 1),"\n"
-print multiple_string('amulya ', 2),"\n"
-print multiple_string('amulya ', 3),"\n"
-print multiple_string('amulya ', 4),"\n"
-print multiple_string('amulya ', 5),"\n"
