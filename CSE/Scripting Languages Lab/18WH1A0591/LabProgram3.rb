e1875ba6b4139a64e269291e267813f0126f88d7
madhumitha357 <51985192+madhumitha357@users.noreply.github.com>
2021-07-09 14:37:48 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A0591/LabProgram3.rb
@@ -0,0 +1,8 @@
+puts "Enter first name: "
+fname = gets.chomp
+puts "Enter last name: "
+lname = gets.chomp
+puts "Before reversing: "
+puts "#{fname} #{lname}"
+puts "After reversing: "
+puts "#{lname} #{fname}"
---
+++ CSE/Scripting Languages Lab/18WH1A0591/LabProgram4.rb
@@ -0,0 +1,18 @@
+puts "Enter the file name"
+file = gets.chomp
+
+#file name
+fbname = File.basename file
+puts "File Name : "+fbname
+
+#base name
+bname = File.basename file,".rb"
+puts "Base Name : "+bname
+
+#file extension 
+fextn = File.extname file
+puts "File Extension : "+fextn
+
+#path name
+pathname = File.dirname file
+puts "Path Name : "+pathname
