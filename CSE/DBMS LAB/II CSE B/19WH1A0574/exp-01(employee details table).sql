cad4e172feef5dcf223e381c97046d2bfb1c31bb
srilalitha-bvrit <69663963+srilalitha-bvrit@users.noreply.github.com>
2021-07-09 11:43:53 +0530

    Add files via upload

---
+++ CSE/DBMS LAB/II CSE B/19WH1A0574/exp-01(employee details table).sql
@@ -0,0 +1,10 @@
+use test;
+create table employee
+(
+id int primary key,
+name varchar(30),
+dob datetime,
+email varchar(40)
+);
+desc employee;
+ 
---
+++ CSE/DBMS LAB/II CSE B/19WH1A0574/insert into key (lab 2).sql
@@ -0,0 +1,17 @@
+desc employee;
+
+insert into employee (id, name, dob, email)
+values(1901, 'lalitha', '2001-06-18 10:00', 'srilalithav576@gmail.com');
+select * from employee;
+
+insert into employee(id, name, dob, email)
+values(2000,'abc', '2013-1-2 12:30:34','adb@gmail.com');
+select * from employee;
+
+insert into employee(id, name, dob, email)
+values(2001,'andy', '2000-1-2 12:32:44','andy@gmail.com');
+select * from employee;
+
+
+
+
