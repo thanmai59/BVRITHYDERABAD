3cfc9c2c014b82dac78b21bb989dc03b8a9dcde9
shreya vodnala <69189998+Shreyavodnala@users.noreply.github.com>
2021-07-09 18:58:26 +0530

    Add files via upload

---
+++ CSE/Scripting Languages Lab/18WH1A05B1/program4.rb
@@ -0,0 +1,19 @@
+puts "Enter File Name"
+
+file = gets.chomp
+
+fbname=File.basename file
+
+puts "File name: " +fbname
+
+bname=File.basename file,".rb"
+
+puts "Base name: " +bname
+
+fextn=File.extname file
+
+puts "Extension:" +fextn
+
+fextn=File.extname file
+
+puts "Extension:" +fextn
